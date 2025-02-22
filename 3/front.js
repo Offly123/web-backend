765589dd21aa88cb4df1a0eb47ec726c66ebce57
Offly123 <kaysinegor@mail.ru>
2025-02-22 16:29:14 +0000

    Всё доделал (верим)

--- 3/front.js
+++ 3/front.js
@@ -4,14 +4,23 @@ $(document).ready(function() {
     $("#" + $(this).attr("for")).prop("checked", true);
   })
 
+
   $("#toggle-languages").click(function() {
     $(".language-list").toggleClass("show-languages");
   });
+  // дальше бога нет
   $(".language-list").on({
     focusout: function(event) {
-      if (!$(event.relatedTarget).closest(".language-list").length) {
+      if ($(event.relatedTarget)[0] == undefined ||
+         ($(event.relatedTarget)[0].className != "language-label" &&
+          $(event.relatedTarget)[0].className != "dont-close"))
         $(".language-list").removeClass("show-languages");
-      }
     }
   });
+  $(document).click(function(event) {
+    if ($(event.target)[0] == undefined ||
+       ($(event.target)[0].className != "dont-close")) {
+         $(".language-list").removeClass("show-languages");
+    }
+  })
 });
