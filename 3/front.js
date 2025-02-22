82374d59ed745d1b4c3111f0a21a9056a0670b02
Offly123 <kaysinegor@mail.ru>
2025-02-22 10:54:20 +0000

    Доделал (нет)

--- 3/front.js
+++ 3/front.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
     e.preventDefault();
     $("#" + $(this).attr("for")).prop("checked", true);
   })
+
+  $("#toggle-languages").click(function() {
+    $(".language-list").toggleClass("show-languages");
+  });
+  $(".language-list").on({
+    focusout: function(event) {
+      if (!$(event.relatedTarget).closest(".language-list").length) {
+        $(".language-list").removeClass("show-languages");
+      }
+    }
+  });
 });
